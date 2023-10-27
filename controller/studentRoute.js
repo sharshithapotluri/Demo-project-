3cbcb600029314e7fc7fd494be8cc1456fee6add
Potluri Sri Harshitha <potlurisriharshitha@gmail.com>
2023-10-27 11:10:00 +0530

    first commit

--- controller/studentRoute.js
+++ controller/studentRoute.js
@@ -1,35 +1,63 @@
-//const Axios = require("axios");
-import Axios from "axios";
-import {Link} from "react-router-dom";
+const express = require("express");
+const studentSchema = require("../model/studentSchema");
+const mongoose = require("mongoose");
 
-function StudentListRow(props)
-{
-    const {_id,name,email,rollNo} = props.obj; //Object destruction
 
-    const handleClick = () => {
-        Axios.delete("http://localhost:4000/students/delete-student/" + _id)
-        .then((res) => {
-            if(res.status === 200){
-                alert("Record deleted successfully");
-                window.location.reload();
-            }
-            else    
-                Promise.reject();
+const studentRoute = express.Router();
+
+
+studentRoute.post("/create-student",(req,res)=>{
+    studentSchema.create(req.body,(err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+
+
+studentRoute.get("/",(req,res)=>{
+    studentSchema.find((err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+studentRoute.route("/update-student/:id")
+.get((req,res)=>{
+    studentSchema.findById(mongoose.Types.ObjectId(req.params["id"]),(err,data)=>{
+        if(err)
+            return err;
+        else{
+            res.json(data);
+        }
+    })
+})
+.put((req,res)=>{
+    studentSchema.findByIdAndUpdate(
+        mongoose.Types.ObjectId(req.params.id),
+        {$set: req.body},
+        (err,data)=>{
+            if(err)
+                return err;
+            else
+                res.json(data);
         })
-        .catch((err)=>alert(err));
-    }
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{email}</td>
-            <td>{rollNo}</td>
-            <td>
-                <button class="btn btn-success ms-3">
-                    <Link class="text-decoration-none text-light" to={"/edit-student/"+ _id}>Edit</Link>
-                </button>
-                <button onClick={handleClick} class="btn btn-danger mx-3">Delete</button>
-            </td>
-        </tr>
-    )
-}
-export default StudentListRow;
+})
+
+
+
+
+studentRoute.delete("/delete-student/:id",(req,res)=>{
+    studentSchema.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id),(err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+
+
+module.exports = studentRoute;
+
